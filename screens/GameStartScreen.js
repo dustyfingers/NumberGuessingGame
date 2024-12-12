1a1f8e68e0e8153e886704d99c6d7aac803a9065
Louie <dustyfingers01257@gmail.com>
2024-12-12 13:47:00 -0700

    learn about ios vs android styles, style the number input

--- screens/GameStartScreen.js
+++ screens/GameStartScreen.js
@@ -5,19 +5,47 @@ import PrimaryButton from "../components/PrimaryButton";
 const GameStartScreen = () => {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={3}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>52
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   inputContainer: {
-    flex: 1,
     padding: 16,
+    marginHorizontal: 24,
     marginTop: 100,
     backgroundColor: "#7206c3",
+    borderRadius: 8,
+    // android box shadow
+    elevation: 4,
+    // ios box shadow
+    shadowColor: "black",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    width: 50,
+    fontSize: 32,
+    padding: 0,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
   },
 });
 
