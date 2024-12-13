7af46da0e0178950eaa2a1c874e686325a1aa097
Louie <dustyfingers01257@gmail.com>
2024-12-13 09:34:26 -0700

    add background to all app views

--- App.js
+++ App.js
@@ -1,9 +1,28 @@
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import GameStartScreen from "./screens/GameStartScreen";
 
 export default function App() {
-  return <GameStartScreen />;
+  return (
+    <LinearGradient style={styles.rootScreen} colors={["#4e0329", "#ddb52f"]}>
+      <ImageBackground
+        source={require("./assets/background.png")}
+        resizeMode="cover"
+        style={styles.rootScreen}
+        imageStyle={styles.backgroundImage}
+      >
+        <GameStartScreen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+  backgroundImage: {
+    opacity: 0.15,
+  },
+});
